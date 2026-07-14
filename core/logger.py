b02caa795362d0bb07689b8163c81137a9ff8978
jaguaridiots-hub <jaguaridiots@gmail.com>
2026-07-14 19:57:02 +0530

    refactor(core): establish logger authority

--- core/logger.py
+++ core/logger.py
@@ -1,11 +1,28 @@
 import logging
 
+
 logging.basicConfig(
-
     level=logging.INFO,
-
     format="%(asctime)s | %(levelname)s | %(message)s"
-
 )
 
-logger = logging.getLogger("Jaguar")
+
+class JaguarLogger:
+
+    def __init__(self, name="Jaguar"):
+        self.logger = logging.getLogger(name)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+
+logger = JaguarLogger()
